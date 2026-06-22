056b8dae887278937a1f7a8c2f15d04f7cd6a3c8
Dhanushree <kldhanushree2@outlook.com>
2026-06-22 10:32:28 +0530

    reverse string

---
+++ reversestring.py
@@ -0,0 +1,14 @@
+"""Given a string, write a function that reverses it. The input string is given as an array of characters s."""
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        i=0
+        j=len(s)-1
+        while(i<=j):
+            s[i],s[j]=s[j],s[i]
+            i+=1
+            j-=1
+            
+        
