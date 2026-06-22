83a8879b0a0a2d9a2c371ae0bee327307b445e9c
Dhanushree <kldhanushree2@outlook.com>
2026-06-22 14:12:59 +0530

    Remove element

---
+++ removeelement.py
@@ -0,0 +1,9 @@
+"""Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The relative order of the elements may be changed."""
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+         k = 0
+         for i in range(0, len(nums)):
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k += 1
+         return k
